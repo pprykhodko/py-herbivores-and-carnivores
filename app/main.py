6293af14383670125a5945bbffe2fda096e0155e
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-02-25 17:13:33 +0100

    solution 3

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 class Animal:
-    alive:list[Animal] = []
+    alive: list[Animal] = []
 
     def __init__(self, name: str, health: int = 100,
                  hidden: bool = False) -> None:
